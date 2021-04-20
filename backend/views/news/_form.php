ae631adea937096f1839fc3b0407ce1603a885ff
webertela <59246555+ubuntu-new@users.noreply.github.com>
2021-04-20 18:43:45 +0400

    04.20.21

--- backend/views/news/_form.php
+++ backend/views/news/_form.php
@@ -4,12 +4,19 @@ use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 use yii\web\View;
 use dosamigos\tinymce\TinyMce;
+use dosamigos\ckeditor\CKEditor;
+use mihaildev\elfinder;
+
+
 
 /* @var $this yii\web\View */
 /* @var $model backend\models\News */
 /* @var $form yii\widgets\ActiveForm */
 $img = $model->getImage();
 $img->getPath();
+$_SESSION['KCFINDER'] = array(
+    'disabled' => false
+);
 ?>
 
 <div class="news-form">
@@ -24,61 +31,35 @@ $img->getPath();
 
     <?= Html::img(Yii::getAlias('@web').'/'.$img->getPath(), ['alt'=>'some', 'class'=>'thing', 'height'=>'150px', 'width'=>'150px'])?>
 
-    <?= $form->field($model, 'description')->widget(TinyMce::className(), [
-        'options' => ['rows' => 6],
-        'language' => 'en',
-        'clientOptions' => [
-            'plugins' => [
-                "advlist autolink lists link image charmap print preview anchor media",
-                "searchreplace visualblocks code fullscreen",
-                "insertdatetime media table contextmenu paste jbimages "
 
-            ],
-            'toolbar' => "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image jbimages media"
-        ]
+
+    <?= $form->field($model, 'description')->widget(CKEditor::className(), [
+        'options' => ['rows' => 8],
+        'clientOptions' =>  elfinder\ElFinder::ckeditorOptions('elfinder',
+            [
+                    'language'=>'en', 'title'=>'Paragraph'
+            ]
+        ),
     ]);?>
 
-    <?= $form->field($model, 'description_ge')->widget(TinyMce::className(), [
-        'options' => ['rows' => 6],
-        'language' => 'en',
-        'clientOptions' => [
-            'plugins' => [
-                "advlist autolink lists link image charmap print preview anchor media",
-                "searchreplace visualblocks code fullscreen",
-                "insertdatetime media table contextmenu paste jbimages "
 
-            ],
-            'toolbar' => "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image jbimages media"
-        ]
+    <?= $form->field($model, 'description_ge')->widget(CKEditor::className(), [
+        'options' => ['rows' => 8],
+        'clientOptions' =>  elfinder\ElFinder::ckeditorOptions('elfinder',
+            [
+                    'language'=>'en', 'title'=>'Paragraph'
+            ]
+        ),
     ]);?>
 
-    <?= $form->field($model, 'metakeyword')->widget(TinyMce::className(), [
-        'options' => ['rows' => 6],
-        'language' => 'en',
-        'clientOptions' => [
-            'plugins' => [
-                "advlist autolink lists link image charmap print preview anchor media",
-                "searchreplace visualblocks code fullscreen",
-                "insertdatetime media table contextmenu paste jbimages "
 
-            ],
-            'toolbar' => "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image jbimages media"
-        ]
-    ]);?>
 
-    <?= $form->field($model, 'metakeyword_ge')->widget(TinyMce::className(), [
-        'options' => ['rows' => 6],
-        'language' => 'en',
-        'clientOptions' => [
-            'plugins' => [
-                "advlist autolink lists link image charmap print preview anchor media",
-                "searchreplace visualblocks code fullscreen",
-                "insertdatetime media table contextmenu paste jbimages "
 
-            ],
-            'toolbar' => "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image jbimages media"
-        ]
-    ]);?>
+
+
+    <?= $form->field($model, 'metakeyword')->textarea(['rows' => 6]);?>
+
+    <?= $form->field($model, 'metakeyword_ge')->textarea(['rows' => 6]);?>
 
 
     <?= $form->field($model, 'feautured')->radioList( [0=>'No', 1 => 'Yes'], ['unselect' => null] ); ?>
@@ -91,7 +72,6 @@ $img->getPath();
     <div class="form-group">
         <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
     </div>
-
     <?php ActiveForm::end(); ?>
 
 </div>
