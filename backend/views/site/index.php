5c750606a53ccfccb2e3575f2eda5aeb1e4a7e51
webertela <59246555+ubuntu-new@users.noreply.github.com>
2021-04-20 22:19:02 +0400

    04.20.2021

--- backend/views/site/index.php
+++ backend/views/site/index.php
@@ -1,5 +1,5 @@
 <?php
-
+use yii\helpers\Html;
 /* @var $this yii\web\View */
 
 $this->title = 'August backery admin pannel';
@@ -16,7 +16,29 @@ $this->title = 'August backery admin pannel';
 
         <div class="row">
             <div class="col-lg-12">
-               &nbsp;
+               &nbsp;<ul class="nav flex-column">
+                    <li class="nav-item">
+                        <?= Html::a('About', ['about/index']) ?>
+                    </li>
+                    <li class="nav-item">
+                        <?= Html::a('User', ['user/index']) ?>
+                    </li>
+                    <li class="nav-item">
+                        <?= Html::a('Products', ['goods/index']) ?>
+                    </li>
+                    <li class="nav-item">
+                        <?= Html::a('Categories', ['categories/index']) ?>
+                    </li>
+                    <li class="nav-item">
+                        <?= Html::a('FAQ', ['faq/index']) ?>
+                    </li>
+                    <li class="nav-item">
+                        <?= Html::a('Locations', ['locations/index']) ?>
+                    </li>
+                    <li class="nav-item">
+                        <?= Html::a('Contact', ['Contact/index']) ?>
+                    </li>
+                </ul>
             </div>
         </div>
 
